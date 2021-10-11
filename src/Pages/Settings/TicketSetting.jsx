fdbd914a95ec3ec93c79fa417924b485ef71cbe4
umair tanoli <ukhalil07@gmail.com>
2021-10-11 17:55:19 +0500

    setting module updated

--- src/Pages/Settings/TicketSetting.jsx
+++ src/Pages/Settings/TicketSetting.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "Layout/Index";
-import { Tabs } from "antd";
+import { Tabs, Button } from "antd";
 function TicketSetting(props) {
   const { TabPane } = Tabs;
   return (
@@ -10,8 +10,29 @@ function TicketSetting(props) {
           <div className="ticket-settings">
             <Tabs defaultActiveKey="1">
               <TabPane tab="SLA" key="1">
-                <div className="d-flex">
-                  <p className="t1">High priority</p>
+                <div className="d-flex justify-content-between">
+                  <div>
+                    <p className="t1">High priority</p>
+                    <p className="t2">
+                      Please define the time and reminder for high priority
+                      tickets
+                    </p>
+                  </div>
+                  <div>
+                    <div class="btns-wrap" style={{ marginTop: "53px" }}>
+                      <Button className="btn-cancel">Reset</Button>
+                      <Button className="btn-create">Update</Button>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="response-time-wrap reminder-high-periority">
+                  <div>
+                    <p className="t1">First </p>
+                    <div className="time-d">
+                      <p className="t3">1d 2hrs</p>
+                    </div>
+                  </div>
                 </div>
               </TabPane>
               <TabPane tab="General" key="2">
